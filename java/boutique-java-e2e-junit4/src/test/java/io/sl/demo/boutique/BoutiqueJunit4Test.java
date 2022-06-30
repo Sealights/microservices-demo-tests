da01487f07e56c07ed5660da08badb19bbd81f45
Volodymyr Hrebinnyi <volodymyr.hrebinnyi@sealights.io>
2022-06-30 19:20:07 +0300

    * Updated tests

--- java/boutique-java-e2e-junit4/src/test/java/io/sl/demo/boutique/BoutiqueJunit4Test.java
+++ java/boutique-java-e2e-junit4/src/test/java/io/sl/demo/boutique/BoutiqueJunit4Test.java
@@ -1,5 +1,6 @@
 package io.sl.demo.boutique;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -7,13 +8,43 @@ import java.io.IOException;
 public class BoutiqueJunit4Test {
     private BoutiqueClientForJunit4 boutiqueClientForJunit4 = new BoutiqueClientForJunit4();
 
-    @Test
-    public void boutiqueSuccessTest() throws IOException {
-        boutiqueClientForJunit4.boutiqueSuccessTest();
+    @BeforeClass
+    public static void before() {
+        BoutiqueUtils.printMethodsForTesting("boutiqueClientForJunit4");
     }
 
     @Test
-    public void boutiqueFailTest() throws IOException {
-        boutiqueClientForJunit4.boutiqueFailTest();
+    public void requestOfMainPageShouldReturnHTMLTest() throws IOException {
+        boutiqueClientForJunit4.requestOfMainPageShouldReturnHTML();
+    }
+
+    @Test
+    public void itShouldCreateOrderTest() throws IOException {
+        boutiqueClientForJunit4.itShouldCreateOrder();
+    }
+
+    @Test
+    public void shouldNotFindProductTest() throws IOException {
+        boutiqueClientForJunit4.shouldNotFindProduct();
+    }
+
+    @Test
+    public void getProductPageTest() throws IOException {
+        boutiqueClientForJunit4.getProductPage();
+    }
+
+    @Test
+    public void requestCartTest() throws IOException {
+        boutiqueClientForJunit4.requestCart();
+    }
+
+    @Test
+    public void requestCartCheckoutTest() throws IOException {
+        boutiqueClientForJunit4.requestCartCheckout();
+    }
+
+    @Test
+    public void requestOfWrongPageShouldReturn404Test() throws IOException {
+        boutiqueClientForJunit4.requestOfWrongPageShouldReturn404();
     }
 }
